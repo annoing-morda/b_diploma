1e2d83baddb476e998776e74daef7e63863dc7ef
morda13 <morozoffda@ya.ru>
2017-06-18 17:25:06 +0300

    added cylinder

--- quadr_programming.py
+++ quadr_programming.py
@@ -1,6 +1,27 @@
 import numpy as np;
 from numpy import linalg as la
 
+class cylinder(object):                     # Класс, описывающий цилиндр
+    def __init__(self, o, a, b, c):         # o - центр основания, a, b - оси эллпса, c - центральная ось цилиндра
+        self.o = o
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def check(self):                                                 # Проверка корректности задания цилиндра
+        if np.matmul(self.a, self.b) == 0:                           # a и b должны быть ортогональны
+            matr = np.hstack(self.a, np.hstack(self.b, self.c))      # a, b и c должны быть ЛНЗ системой
+            if la.det(matr) != 0:
+                return True
+        return False
+
+    def get_translation(self):                                       # Возвращает вектор параллельного переноса (для смены базиса)
+        return -o
+
+
+
+
+
 def calculate_form_value(M, b, vec):
     res = np.matmul(np.transpose(vec), np.matmul(M, vec)) + np.matmul(np.transpose(b), vec)
     return res[0, 0]
@@ -53,7 +74,7 @@ def solve_2dim(M, b, cond, cond_const):  # Here we minimize function M(x, x) - b
     minval = 100
     minpoint = np.array([[-1000], [-1000]])
     for i in range(cond_const.shape[0]):
-        (curpoint, curval) = solve_1dim(M, b, 
+        (curpoint, curval) = solve_1dim(M, b,
             np.delete(cond, i, 0), np.delete(cond_const, i, 0),
             cond[i], cond_const[i, 0] )
         if curval < minval:
@@ -61,7 +82,7 @@ def solve_2dim(M, b, cond, cond_const):  # Here we minimize function M(x, x) - b
             minpoint = curpoint
     return (minpoint, minval)
 
-print(solve_2dim(np.array([[1, 0], [0, 1]]), 
-    np.array([[1], [2]]), 
+print(solve_2dim(np.array([[1, 0], [0, 1]]),
+    np.array([[1], [2]]),
     np.array([[1, 0], [-1, 0], [0, 1], [0, -1], [1, 1], [-1, -1]]),
     np.array([[0], [1], [0], [1], [-1], [1]]) ))
